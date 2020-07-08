dc6e8660d66740acfd24fa2123db9cb8e07f1468
nitin katiyar <nkatiyar11@gmail.com>
2020-07-08 14:00:15 +0530

    added part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -x
 
-
+   partDayHour=8
 	wagePerHour=20
+   partWagePerHour=10;
 	fullDayHour=8
+   full_time=1
+   part_time=0
    	random=$((RANDOM % 2))
-     		if [ $random -eq 1 ]
+     		if [ $random -eq $full_time ]
 	then
-    	 	echo "present"
+
 			dailyEmployeeWage=$(($wagePerHour * $fullDayHour))
      		echo "dailyEmployeeWage:"$dailyEmployeeWage
 	else
-	  		echo "Absent"
-			fullDayHour=$(($fullDayHour * 0))
-			dailyEmployeeWage=$(($wagePerHour * $fullDayHour))
+
+			dailyEmployeeWage=$(($partWagePerHour * $partDayHour))
 			echo "dailyEmployeeWage:"$dailyEmployeeWage
 	fi
