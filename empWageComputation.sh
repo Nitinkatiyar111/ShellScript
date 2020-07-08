ad318f090f184e3d8cbc177005544411da5398e1
nitin katiyar <nkatiyar11@gmail.com>
2020-07-09 02:28:39 +0530

    couting hours using function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,35 +1,30 @@
 #!/bin/bash -x
-partDayHour=8
-wagePerHour=20
-partWagePerHour=10;
-fullDayHour=8
 full_time=1
 part_time=0
-workingDay=1
-workingHour=0
-maxWorkingDay=20
-maxWorkingHour=100
 
-				while [[ $workingDay < $maxWorkingDay || $workingHour < $maxWorkingHour ]]
-	do
-               random=$((RANDOM % 2))
 
-		case $random in
-			$full_time)
-                     dailyEmployeeWage=$(($wagePerHour * $fullDayHour))
-                     echo "dailyFullTimeEmployeeWage:"$dailyEmployeeWage
+empHours(){
 
-                    ;;
-         $part_time)
-                     dailyEmployeeWage=$(($partWagePerHour * $partDayHour))
-                      echo "dailyPartTimeEmplyeeWage:"$dailyEmployeeWage
+           random=$((RANDOM % 2))
+			if [ $random -eq 0 ];
+	then
+         empHr=4
+   else
+			empHr=8
+	fi
+echo "$empHr"
+}
 
-                    ;;
+result=$(empHours)
+echo $result;
 
-           esac
-							workingHour=`expr $workingHour + 8`
-							 workingDay=`expr $workingDay + 1`
-		done
+
+
+
+
+
+
+
 
 
 
